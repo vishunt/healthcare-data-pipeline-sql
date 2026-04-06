7f8aff6481162e55328cd7988f6c89cc25d5aaff
vishunt <vishnuvardh001@gmail.com>
2026-04-06 16:18:57 +0530

    Implemented SCD Type 2 logic with change detection and history tracking

--- queries/load_patients.sql
+++ queries/load_patients.sql
@@ -1,24 +1,45 @@
 USE HealthcareLab;
 
-DECLARE @InsertedCount INT;
 DECLARE @LastLoadDate DATETIME;
 
--- Get last processed time
 SELECT @LastLoadDate = ISNULL(MAX(LastProcessedDate), '1900-01-01')
 FROM Load_Log;
 
--- 🔥 STEP 1: UPDATE existing records (based on PatientID)
+--------------------------------------------------
+-- STEP 1: EXPIRE OLD RECORDS
+--------------------------------------------------
+WITH RankedPatients AS
+(
+    SELECT
+        PatientID,
+        Name,
+        DOB,
+        Gender,
+        ROW_NUMBER() OVER (
+            PARTITION BY PatientID
+            ORDER BY LoadDate DESC
+        ) AS rn
+    FROM Staging_Patients
+)
+
 UPDATE p
 SET 
-    p.Name = s.Name,
-    p.DOB = s.DOB,
-    p.Gender = s.Gender
+    EndDate = GETDATE(),
+    IsActive = 0
 FROM Patients p
-JOIN Staging_Patients s
-    ON p.PatientID = s.PatientID
-WHERE s.LoadDate > DATEADD(DAY, -2, @LastLoadDate);
+JOIN RankedPatients r
+    ON p.PatientID = r.PatientID
+WHERE p.IsActive = 1
+AND r.rn = 1
+AND (
+    p.Name <> r.Name OR
+    p.DOB <> r.DOB OR
+    p.Gender <> r.Gender
+);
 
--- 🔥 STEP 2: INSERT only NEW PatientIDs
+--------------------------------------------------
+-- STEP 2: INSERT NEW VERSION
+--------------------------------------------------
 WITH RankedPatients AS
 (
     SELECT
@@ -32,26 +53,27 @@ WITH RankedPatients AS
             ORDER BY LoadDate DESC
         ) AS rn
     FROM Staging_Patients
-    WHERE DOB IS NOT NULL
-      AND LoadDate > DATEADD(DAY, -2, @LastLoadDate)
 )
 
-INSERT INTO Patients (PatientID, Name, DOB, Gender)
+INSERT INTO Patients (PatientID, Name, DOB, Gender, EffectiveDate, EndDate, IsActive)
 SELECT
     r.PatientID,
     r.Name,
     r.DOB,
-    r.Gender
+    r.Gender,
+    GETDATE(),
+    NULL,
+    1
 FROM RankedPatients r
 WHERE r.rn = 1
 AND NOT EXISTS (
     SELECT 1
     FROM Patients p
     WHERE p.PatientID = r.PatientID
+      AND p.IsActive = 1
 );
-
-SET @InsertedCount = @@ROWCOUNT;
-
--- 🔥 STEP 3: LOG
+--------------------------------------------------
+-- STEP 3: LOG
+--------------------------------------------------
 INSERT INTO Load_Log (RecordsInserted, LastProcessedDate)
-VALUES (@InsertedCount, GETDATE());
+VALUES (0, GETDATE());
